b54372f56f7a578c2058a021d0b87980ab278da3
BRC3894A <BRC3894A@U3-209-03.ens.fsi>
2023-04-14 09:38:59 +0200

    tp3 9/16

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,9 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private int nbTrophees;
+	private Equipement trophees[] = new Equipement[100];
+	
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -18,15 +21,27 @@ public class Gaulois {
 		System.out.println(prendreParole() + "<" + texte + ">");	
 	}
 	
+//	private String prendreParole() {
+//		return "Le gaulois" + nom + ":";
+//	}
+	
 	private String prendreParole() {
-		return "Le gaulois" + nom + ":";
+		return "Le gaulois " + nom + " : ";
 	}
 	
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + "envoie un grand coup dans la mâchoire de" + romain.getNom());
+//		romain.recevoirCoup((force/3) * effetPotion);
+//	}
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + "envoie un grand coup dans la mâchoire de" + romain.getNom());
-		romain.recevoirCoup((force/3) * effetPotion);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de" + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,nbTrophees++) {
+			this.trophees[nbTrophees] = trophees[i];
+		}
 	}
-	@Override
+	
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
